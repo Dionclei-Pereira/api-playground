0cc15deabfc7e388b9c3d4f5408fe04d02f7a53b
Dionclei-Pereira <dionclei2@gmail.com>
2026-09-15 10:45:40 -0300

    feat(home): add index page header and footer

--- resources/js/views/home.index.ts
+++ resources/js/views/home.index.ts
@@ -1,2 +1,10 @@
-const text: string = "Hello world";
-console.log(text);
+document.addEventListener('DOMContentLoaded', async () => {
+    await loadFooter();
+});
+
+const loadFooter = async () => {
+    const githubBadge: HTMLImageElement = document.getElementById('github') as HTMLImageElement;
+    githubBadge.addEventListener('click', () => {
+        window.location.href = 'https://github.com/Dionclei-Pereira/api-playground';
+    });
+}
