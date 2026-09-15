37e678068f085d15c986fcdc6128833c66b01384
Dionclei-Pereira <dionclei2@gmail.com>
2026-09-15 13:21:05 -0300

    style(footer): change bootstrap class from 'fixed-bottom' to 'bottom-0'

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="fixed-bottom container-fluid d-flex align-items-center flex-column gap-2">
+<footer class="bottom-0 mt-3 container-fluid d-flex align-items-center flex-column gap-2">
     <div>
         <img data-url="{{ $githubUrl }}" id="github"
             src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white"
